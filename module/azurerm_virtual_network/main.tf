82b17e3c6d7fed996634075842700e10eeb4fe57
A K Verma <amitkkc01@gmail.com>
2025-09-01 00:35:11 +0530

    Update main.tf

--- module/azurerm_virtual_network/main.tf
+++ module/azurerm_virtual_network/main.tf
@@ -3,5 +3,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
   resource_group_name = var.rg_name
   address_space       = var.address_space
+  dns_servers         = ["8.8.8.8", "8.8.4.4"]
+
 
 }
