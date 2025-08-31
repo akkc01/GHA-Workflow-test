df4b5608073d7fd3325d5ee4c50f479b55f82397
akkc01 <amitkkc01@gmail.com>
2025-08-31 16:09:22 +0530

    add all new code

---
+++ Environment/Dev/main.tf
@@ -0,0 +1,27 @@
+module "rg" {
+  source   = "../../Module/azurerm_resource_group"
+  rg_name  = "varddha-rg001"
+  location = "centralindia"
+}
+
+module "vnet" {
+  source        = "../../module/azurerm_virtual_network"
+  depends_on    = [module.rg]
+  vnet_name     = "varddha-vnet001"
+  address_space = ["192.168.4.0/24"]
+  rg_name       = "varddha-rg001"
+  location      = "centralindia"
+
+}
+
+module "subnet" {
+  source            = "../../module/azurerm_subnet"
+  depends_on        = [module.rg, module.vnet]
+  fsub_name         = "Khanshaar-Frontend"
+  rg_name           = "varddha-rg001"
+  address_prefixes  = ["192.168.4.0/26"]
+  vnet_name         = "varddha-vnet001"
+  bsub_name         = "Khanshaar-Backend"
+  address_prefixes1 = ["192.168.4.64/26"]
+}
+
